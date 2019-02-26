e16de09014436367c85b8d003ea03cb193e41d8c
Marco Mirizio <damn.miri@gmail.com>
2019-02-26 03:39:48 +0100

    heroku fix

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -14,7 +14,7 @@ import java.net.URI;
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
     //public static final String BASE_URI = "http://localhost:8080/myapp/";
-    public static final String BASE_URI = "graph-recommender.herokuapp.com";
+    public static String BASE_URI;
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
@@ -26,6 +26,7 @@ public class Main {
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
+        BASE_URI = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"8080")+"/";
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
