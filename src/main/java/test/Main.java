eec7221bad90553730109662530ec9d8a0fc93c7
Marco Mirizio <damn.miri@gmail.com>
2019-02-26 04:19:40 +0100

    heroku fix

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -14,7 +14,7 @@ import java.net.URI;
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
     //public static final String BASE_URI = "http://localhost:8080/myapp/";
-    public static String BASE_URI;
+    static String url;
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
@@ -23,10 +23,11 @@ public class Main {
         // create a resource config that scans for JAX-RS resources and providers
         // in test package
         final ResourceConfig rc = new ResourceConfig().packages("test");
-
+        String host = "0.0.0.0";
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        BASE_URI = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"8080")+"/";
+        final String BASE_URI = "http://"+host+":"+(System.getenv("PORT")!=null?System.getenv("PORT"):"8080")+"/";
+        url=BASE_URI;
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
@@ -38,7 +39,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl", BASE_URI));
+                + "%sapplication.wadl", url));
 
         while (true) {
 
